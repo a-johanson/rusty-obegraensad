abbd8f9b9182431e5054d612a614cb97639a026b
a-johanson <a-johanson@users.noreply.github.com>
2024-04-25 22:05:48 +0200

    try to use spi fed via dma to transfer display content

--- src/display.rs
+++ src/display.rs
@@ -1,25 +1,15 @@
 
-use embedded_hal::digital::{OutputPin, PinState};
-
 const DISPLAY_SIZE: usize = 16;
 const BYTE_COUNT: usize = 2 * DISPLAY_SIZE;
 
-pub struct ObegraensadDisplay<CLK: OutputPin, DI: OutputPin, LA: OutputPin> {
-    pixels: [u8; BYTE_COUNT],
-    pin_clock: CLK,
-    pin_data: DI,
-    pin_latch: LA,
-    delay: cortex_m::delay::Delay,
+pub struct ObegraensadDisplay {
+    pixels: [u8; BYTE_COUNT]
 }
 
-impl<CLK: OutputPin, DI: OutputPin, LA: OutputPin> ObegraensadDisplay<CLK, DI, LA> {
-    pub fn new(pin_clock: CLK, pin_data: DI, pin_latch: LA, delay: cortex_m::delay::Delay) -> Self {
+impl ObegraensadDisplay {
+    pub fn new() -> Self {
         Self {
-            pixels: [0; BYTE_COUNT],
-            pin_clock,
-            pin_data,
-            pin_latch,
-            delay,
+            pixels: [0; BYTE_COUNT]
         }
     }
 
@@ -40,31 +30,8 @@ impl<CLK: OutputPin, DI: OutputPin, LA: OutputPin> ObegraensadDisplay<CLK, DI, L
         self.pixels[byte_index as usize] |= 1 << bit_index;
     }
 
-    pub fn show(&mut self) {
-        for byte in self.pixels {
-            self.output_byte(byte);
-        }
-        self.pin_latch.set_high().unwrap();
-        // Delay for at least 20 ns
-        self.delay.delay_us(10);
-        self.pin_latch.set_low().unwrap();
-    }
-
-    fn output_byte(&mut self, mut byte: u8) { // Replace bit banging with SPI peripheral in the future
-        for _ in 0..8 {
-            let value = byte & 0b1000_0000 != 0;
-            self.pin_data.set_state(match value {
-                true => PinState::High,
-                _ => PinState::Low,
-            }).unwrap();
-            // Delay for at least 5 ns
-            self.delay.delay_us(5);
-            self.pin_clock.set_high().unwrap();
-            // Delay for at least 20 n
-            self.delay.delay_us(5);
-            self.pin_clock.set_low().unwrap();
-            byte = byte << 1;
-        }
+    pub fn to_output_buffer(&self, buffer: &mut [u8; BYTE_COUNT]) {
+        buffer.copy_from_slice(&self.pixels);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 mod display;
 
-use core::cell::{Cell, RefCell};
+use core::cell::RefCell;
 
 use rp_pico::entry;
 use panic_halt as _;
@@ -12,24 +12,53 @@ use rp_pico::hal; // Hardware Abstraction Layer (higher-level drivers)
 use rp_pico::hal::pac; // Peripheral Access Crate (low-level register access)
 use rp_pico::hal::pac::interrupt;
 use rp_pico::hal::timer::{Alarm, Alarm0};
-use rp_pico::hal::gpio::PinState;
+use rp_pico::hal::gpio::{FunctionSpi, PinState};
+use rp_pico::hal::dma::{single_buffer, SingleChannel, DMAExt};
 use rp_pico::hal::Clock;
 use embedded_hal::digital::OutputPin;
-use cortex_m::interrupt::{CriticalSection, Mutex};
+use cortex_m::interrupt::Mutex;
+use cortex_m::singleton;
 
+use portable_atomic::{AtomicU32, AtomicU8, Ordering};
 use fugit::MicrosDurationU32;
-// use fugit::RateExtU32;
+use fugit::RateExtU32;
 
 // TODO: look at https://github.com/knurling-rs/flip-link
 
-static ALARM0: Mutex<RefCell<Option<Alarm0>>> = Mutex::new(RefCell::new(None));
-static FRAME_DURATION: Mutex<Cell<MicrosDurationU32>> = Mutex::new(Cell::new(MicrosDurationU32::millis(500)));
-static TRANSMIT_FRAME: Mutex<Cell<bool>> = Mutex::new(Cell::new(false));
+static SHARED_STATE: Mutex<RefCell<Option<SharedState>>> = Mutex::new(RefCell::new(None));
+static ATOMIC_STATE: AtomicState = AtomicState::new();
+
+type DisplaySpi = hal::Spi<hal::spi::Enabled, pac::SPI0, (hal::gpio::Pin<hal::gpio::bank0::Gpio19, hal::gpio::FunctionSpi, hal::gpio::PullDown>, hal::gpio::Pin<hal::gpio::bank0::Gpio18, hal::gpio::FunctionSpi, hal::gpio::PullDown>)>;
+type SpiDmaChannel =  hal::dma::Channel<hal::dma::CH0>;
+type SpiDmaTransfer = single_buffer::Transfer<hal::dma::Channel<hal::dma::CH0>, &'static mut [u8; 32], hal::Spi<hal::spi::Enabled, pac::SPI0, (hal::gpio::Pin<hal::gpio::bank0::Gpio19, FunctionSpi, hal::gpio::PullDown>, hal::gpio::Pin<hal::gpio::bank0::Gpio18, FunctionSpi, hal::gpio::PullDown>)>>;
+type LatchPin = hal::gpio::Pin<hal::gpio::bank0::Gpio21, hal::gpio::FunctionSio<hal::gpio::SioOutput>, hal::gpio::PullDown>;
+
+struct SharedState {
+    alarm0: Alarm0,
+    spi: Option<DisplaySpi>,
+    dma_channel: Option<SpiDmaChannel>,
+    dma_buffer: Option<&'static mut [u8; 32]>,
+    spi_dma_transfer: Option<SpiDmaTransfer>,
+    pin_latch: LatchPin,
+}
+
+struct AtomicState {
+    frame_duration: AtomicU32,
+    transmit_next_frame: AtomicU8,
+}
+
+impl AtomicState {
+    pub const fn new() -> Self {
+        Self {
+            frame_duration: AtomicU32::new(MicrosDurationU32::millis(500).to_micros()),
+            transmit_next_frame: AtomicU8::new(0),
+        }
+    }
+}
 
 #[entry]
 fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
 
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
 
@@ -45,8 +74,6 @@ fn main() -> ! {
         &mut watchdog,
     ).unwrap();
 
-    let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-
     // Set up peripherals (GPIO, Timer/Alarm, and maybe SPI for data transmission)
     let sio = hal::Sio::new(pac.SIO); // single-cycle IO
     let pins = rp_pico::Pins::new(
@@ -55,45 +82,60 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-    let mut pin_led = pins.led.into_push_pull_output_in_state(PinState::High);
     let mut pin_not_enable = pins.gpio20.into_push_pull_output_in_state(PinState::High);
+    let mut pin_led = pins.led.into_push_pull_output_in_state(PinState::High);
     let pin_latch = pins.gpio21.into_push_pull_output_in_state(PinState::Low);
-    let pin_clock = pins.gpio18.into_push_pull_output_in_state(PinState::Low);
-    let pin_data = pins.gpio19.into_push_pull_output();
+    // let pin_clock = pins.gpio18.into_push_pull_output_in_state(PinState::Low);
+    // let pin_data = pins.gpio19.into_push_pull_output();
 
-    let mut display = display::ObegraensadDisplay::new(pin_clock, pin_data, pin_latch, delay);
+    // TODO: set power regulator mode (PFM (low; default) vs PWM (high)) controlled via GPIO23 (pins.b_power_save)
 
-    // let spi_clk: hal::gpio::Pin<_, hal::gpio::FunctionSpi, hal::gpio::PullNone> = pins.gpio18.reconfigure();
-    // let spi_tx: hal::gpio::Pin<_, hal::gpio::FunctionSpi, hal::gpio::PullNone> = pins.gpio19.reconfigure();
+    // let core = pac::CorePeripherals::take().unwrap();
+    // let delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    let pin_clock = pins.gpio18.into_function::<FunctionSpi>();
+    let pin_data = pins.gpio19.into_function::<FunctionSpi>();
     // let spi_rx: hal::gpio::Pin<_, hal::gpio::FunctionSpi, hal::gpio::PullUp> = pins.gpio16.reconfigure();
     // let spi_cs = pins.gpio17.into_push_pull_output();
-    // let spi = hal::spi::Spi::<_, _, _, 8>::new(pac.SPI0, (spi_tx, spi_clk)); // TODO: omit spi_rx in (spi_tx, spi_rx, spi_clk)?
-    // let spi = spi.init(
-    //     &mut pac.RESETS,
-    //     clocks.peripheral_clock.freq(),
-    //     500.kHz(),
-    //     embedded_hal::spi::MODE_0,
-    // );
-    // spi.write(words);
+    let spi = hal::spi::Spi::<_, _, _, 8>::new(pac.SPI0, (pin_data, pin_clock));
+    let spi = spi.init(
+        &mut pac.RESETS,
+        clocks.peripheral_clock.freq(),
+        500.kHz(),
+        embedded_hal::spi::MODE_0,
+    );
 
-    // pin_led.set_high().unwrap();
-    // TODO: set power regulator mode (PFM (low; default) vs PWM (high)) controlled via GPIO23 (pins.b_power_save)
+    // Initialize DMA
+    let dma_buffer = singleton!(: [u8; 32] = [0; 32]).unwrap();
+    let dma = pac.DMA.split(&mut pac.RESETS);
+    let mut dma_channel = dma.ch0;
+    dma_channel.enable_irq0();
+    // transmit empty frame
+    let spi_dma_transfer = single_buffer::Config::new(dma_channel, dma_buffer, spi).start();
 
     let mut timer = hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
     let mut alarm0 = timer.alarm_0().unwrap();
     alarm0.enable_interrupt();
+
     cortex_m::interrupt::free(|cs| {
-        ALARM0.borrow(cs).replace(Some(alarm0));
-        reschedule_alarm(cs);
+        SHARED_STATE.borrow(cs).replace(Some(SharedState {
+            alarm0,
+            spi: None,
+            dma_channel: None,
+            dma_buffer: None,
+            spi_dma_transfer: Some(spi_dma_transfer),
+            pin_latch
+        }));
     });
+
+    reschedule_alarm0();
 
     unsafe {
         pac::NVIC::unmask(pac::Interrupt::TIMER_IRQ_0);
+        pac::NVIC::unmask(pac::Interrupt::DMA_IRQ_0);
     }
 
-    // transmit empty frame
-    display.show();
-    pin_not_enable.set_low().unwrap();
+    let mut display = display::ObegraensadDisplay::new();
     let mut frame_count: u32 = 0;
     loop {
         // compute next frame
@@ -102,33 +144,61 @@ fn main() -> ! {
         let x = pixel_idx & 0x0F;
         let y = pixel_idx >> 4;
         display.set_pixel(x, y);
+
         frame_count += 1;
 
         // sleep until next frame should be transmitted
-        while !cortex_m::interrupt::free(|cs| TRANSMIT_FRAME.borrow(cs).get()) {
-            pin_led.set_low().unwrap();
+        pin_led.set_low().unwrap();
+        while ATOMIC_STATE.transmit_next_frame.load(Ordering::Relaxed) == 0 {
             cortex_m::asm::wfi();
         }
-        cortex_m::interrupt::free(|cs| TRANSMIT_FRAME.borrow(cs).set(false));
+        ATOMIC_STATE.transmit_next_frame.store(0, Ordering::Relaxed);
         pin_led.set_high().unwrap();
+        pin_not_enable.set_low().unwrap();
 
         // transmit frame
-        display.show();
+        cortex_m::interrupt::free(|cs| {
+            let mut shared_state = SHARED_STATE.borrow(cs).borrow_mut();
+            let dma_buffer = shared_state.as_mut().map(|s| s.dma_buffer.take().unwrap()).unwrap();
+            display.to_output_buffer(dma_buffer);
+            let dma_channel = shared_state.as_mut().map(|s| s.dma_channel.take().unwrap()).unwrap();
+            let spi = shared_state.as_mut().map(|s| s.spi.take().unwrap()).unwrap();
+            let spi_dma_transfer = single_buffer::Config::new(dma_channel, dma_buffer, spi).start();
+            shared_state.as_mut().map(|s| s.spi_dma_transfer.replace(spi_dma_transfer));
+        });
     }
 }
 
-fn reschedule_alarm(cs: &CriticalSection) {
-    let duration = FRAME_DURATION.borrow(cs).get();
-    let mut alarm = ALARM0.borrow(cs).borrow_mut();
-    let alarm = alarm.as_mut().unwrap();
-    alarm.schedule(duration).unwrap();
-    alarm.clear_interrupt();
+fn reschedule_alarm0() {
+    let frame_mircos = ATOMIC_STATE.frame_duration.load(Ordering::Relaxed);
+    cortex_m::interrupt::free(|cs| {
+        let mut shared_state = SHARED_STATE.borrow(cs).borrow_mut();
+        let alarm = &mut shared_state.as_mut().unwrap().alarm0;
+        alarm.schedule(MicrosDurationU32::micros(frame_mircos)).unwrap();
+        alarm.clear_interrupt();
+    });
 }
 
 #[interrupt]
 fn TIMER_IRQ_0() {
+    ATOMIC_STATE.transmit_next_frame.store(1, Ordering::Relaxed);
+    reschedule_alarm0();
+}
+
+#[interrupt]
+fn DMA_IRQ_0() {
     cortex_m::interrupt::free(|cs| {
-        TRANSMIT_FRAME.borrow(cs).set(true);
-        reschedule_alarm(cs);
+        let mut shared_state = SHARED_STATE.borrow(cs).borrow_mut();
+        let mut spi_dma_transfer = shared_state.as_mut().map(|s| s.spi_dma_transfer.take().unwrap()).unwrap();
+        spi_dma_transfer.check_irq0();
+        let (dma_channel, dma_buffer, spi) = spi_dma_transfer.wait();
+        shared_state.as_mut().unwrap().pin_latch.set_high().unwrap();
+        shared_state.as_mut().map(|s| s.spi.replace(spi));
+        shared_state.as_mut().map(|s| s.dma_channel.replace(dma_channel));
+        shared_state.as_mut().map(|s| s.dma_buffer.replace(dma_buffer));
+        cortex_m::asm::nop();
+        cortex_m::asm::nop();
+        cortex_m::asm::nop();
+        shared_state.as_mut().unwrap().pin_latch.set_low().unwrap();
     });
 }
