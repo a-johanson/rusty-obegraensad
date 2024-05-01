ca1d0c44833b36a12391e29d5b21afa7cb16d4c6
a-johanson <a-johanson@users.noreply.github.com>
2024-05-01 11:03:49 +0200

    add explanation to the animation trait

--- src/animation.rs
+++ src/animation.rs
@@ -3,5 +3,8 @@ use crate::display::ObegraensadDisplay;
 use fugit::MicrosDurationU32;
 
 pub trait Animation {
+    /// Renders the next frame of the animation to the display and returns the duration this frame should be displayed for.
+    /// 
+    /// When implementing this method, you typically want to use `display.clear()` to erase the current contents of the display and then draw your frame using `display.set_pixel()`.
     fn render_frame(&mut self, display: &mut ObegraensadDisplay) -> MicrosDurationU32;
 }
